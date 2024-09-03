ce1e14d5f6443bb3c9987e541cfff52e01c55b9f
rpmisko <rpmisko@gmail.com>
2024-09-03 18:56:51 +0300

    Wrote database package

---
+++ database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"time"
+
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/lib/pq"
+)
+
+type Card struct {
+	Id            int64     `db:"id"`
+	Front         string    `db:"front"`
+	Back          string    `db:"back"`
+	UserId        int       `db:"user_id"`
+	DateExpired   time.Time `db:"date_expired"`
+	CurrentStrike int       `db:"current_strike"`
+}
+
+func Create(db *sqlx.DB, front, back string, user_id int) error {
+	card := &Card{Front: front, Back: back, UserId: user_id, DateExpired: time.Now().AddDate(0, 0, 1), CurrentStrike: 1}
+	_, err := db.NamedExec("insert into cards (front, back, user_id, date_expired, current_strike) values(:front, :back, :user_id, :date_expired, :current_strike)", &card)
+	return err
+}
+
+func Update(db *sqlx.DB, front, back string, user_id int) error {
+	_, err := db.Exec("update cards set back = $2 where front = $1 and user_id = $3", front, back, user_id)
+	return err
+}
+
+func Get(db *sqlx.DB, user_id int) ([]Card, error) {
+	cards := []Card{}
+	err := db.Select(&cards, "select * from cards where user_id = $1 and date_expired <= $2", user_id, time.Now())
+	return cards, err
+}
+
+func Delete(db *sqlx.DB, front string, user_id int) error {
+	_, err := db.Exec("delete from cards * where front = $1 and user_id = $2", front, user_id)
+	return err
+}
+
+func Init() (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", "postgres://card_admin:password@localhost/cards?sslmode=disable")
+	return db, err
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/BlackGoose/flashBot
+
+go 1.22.2
+
+require (
+	github.com/go-telegram/bot v1.6.1 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
